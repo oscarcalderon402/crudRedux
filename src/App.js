4642ef5117428c736fe8d6a39bcb04e5058e12ad
Oscar Calderon <gregoriocalderon.07@hotmail.es>
2021-03-30 07:40:52 -0400

    redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 // import React, { Fragment } from 'react';
 import Header from './components/Header';
 
+import Productos from './components/Productos';
+import NuevoProducto from './components/NuevoProductos';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import EditarProducto from './components/EditarProducto';
+
 function App() {
-  return <Header />;
+  return (
+    <Router>
+      <Header />
+      <div className="container mt-5">
+        <Switch>
+          <Route exact path="/" component={Productos} />
+          <Route exact path="/productos/nuevos" component={NuevoProducto} />
+          <Route
+            exact
+            path="/productos/editar/:id"
+            component={EditarProducto}
+          />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
